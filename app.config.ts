bef9487d69d7e5280af20b7f43696d9ffd58c0dd
LuisBrescia <luisfelipebrescia@gmail.com>
2024-04-11 23:58:03 -0300

    Removido registros de cards em dispositivos sm

--- app.config.ts
+++ app.config.ts
@@ -10,6 +10,8 @@ export default defineAppConfig({
       background: "sm:bg-white sm:dark:bg-gray-900",
       rounded: "rounded-sm",
       ring: "sm:ring-1 ring-gray-200 dark:ring-gray-800",
+      shadow: "sm:shadow-sm",
+      divide: "sm:divide-y divide-gray-200 dark:divide-gray-800",
     },
     formGroup: {
       label: {
